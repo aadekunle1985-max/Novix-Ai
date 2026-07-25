a63ed6b4b61ea2b84091d66c4eacb2132c206547
aadekunle1985-max <aadekunle1985@gmail.com>
2026-07-25 22:01:49 +0100

    Update script.js

--- script.js
+++ script.js
@@ -62,3 +62,12 @@ userInput.addEventListener("keydown", (e) => {
         sendMessage();
     }
 });
+
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker
+      .register("/service-worker.js")
+      .then(() => console.log("Service Worker Registered"))
+      .catch((err) => console.log(err));
+  });
+}
